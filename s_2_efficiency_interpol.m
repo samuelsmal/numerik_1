186a86470a962259d098b992ab671509ac20c410
samuelsmal <samuel.vonbaussnern@gmail.com>
2014-03-06 11:15:18 +0100

    Corrected estimate.

--- s_2_efficiency_interpol.m
+++ s_2_efficiency_interpol.m
@@ -1,11 +1,11 @@
 data = 1:20;
 
-newton = data .^4;
-lagrange = data .^3;
+newton = (data.^2 + data) ./ 2 + data.*2 - 1;
+lagrange = data.^2 + data;
 
 figure(1)
 plot(data, newton, 'b--', data, lagrange, 'g:')
-title('Worst-case comparision of Newton and Lagrange interpolation')
+title('Comparison of Newton and Lagrange interpolation')
 xlabel('Number of data points')
 ylabel('Amount of multiplications')
 
